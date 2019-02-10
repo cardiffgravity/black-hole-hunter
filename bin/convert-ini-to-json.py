d503ee4d9943eb563bcbd1899f69166682a44398
McIsaacC <mcisaacc@c7l9829a64561dc.broadband>
2019-02-10 23:16:13 +0000

    I have added in new files using O1 data and reoranised these slightly. There is now a bank of noise files, in that folder there are also glitches that could be used later. The signal bank is organised in much the same way, I will at some point add the signals in glitchy noise. the signalBank json file has changed slightly, it now holds spin information as well, it has information on the number of each type of file and a few variable that are part of how the data generation functions. A new element has been added to the html and css files to display the spin, this could probablt be done in a more intuitive way. The coffee file has been changed so that the signals and noise files are selected from the full list using the metadata in the json file, this saves changing it any time the data files change. The convert-to-ini file was changed so that empty entries are added to the locs json file for any that exist in one language but not another, and the missing entries are printed when it is run. This was done as it seemed to cause some strange issues when this was not done. (this bit of code could be a bit neater)

--- bin/convert-ini-to-json.py
+++ bin/convert-ini-to-json.py
@@ -34,6 +34,37 @@ for l in locList:
             for k, v in cp.items(section):
                 locs[l][n][section][k] = v
 
+for n in fileList:
+	sections = []
+	entries = {}
+	
+	# loop through localisations and find all sections and entries within them
+	for l in locList:
+		for section in locs[l][n].keys():
+			if not section in sections:
+				sections.append(section)
+				entries[section] = []
+				for entry in locs[l][n][section].keys():
+					entries[section].append(entry)
+			else:
+				for entry in locs[l][n][section].keys():
+					if not entry in entries[section]:
+						entries[section].append(entry)
+	
+	# loop through localisations and create empty entries for those missing
+	for l in locList:
+		for section in sections:
+			if not section in locs[l][n].keys():
+				print 'file = {0: <15}, section = {1: <15}{3}is missing from {2}'.format(n, section, l, ' '*25)
+				locs[l][n][section] = {}
+				for entry in entries[section]:
+					locs[l][n][section][entry] = ''
+			else:
+				for entry in entries[section]:
+					if not entry in locs[l][n][section].keys():
+						print 'file = {0: <15}, section = {1: <15}, entry = {2: <15}is missing from {3}'.format(n, section, entry, l)
+						locs[l][n][section][entry] = ''
+
 #
 # Dump loc to file
 #
